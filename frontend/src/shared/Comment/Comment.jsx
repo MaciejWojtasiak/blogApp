00b44187d6a92de762964204c7413aa10019b41c
Maciej Wojtasiak <maciekwojtasiak@gmail.com>
2023-07-27 18:47:04 +0200

    working on comment component

--- frontend/src/shared/Comment/Comment.jsx
+++ frontend/src/shared/Comment/Comment.jsx
@@ -1,10 +1,21 @@
-import React from 'react';
+import React , {useContext}from 'react';
 import UserAvatar from '../UserAvatar/UserAvatar';
-import './Comment.css'
+import './Comment.css';
+import { Context } from '../../context/Context';
 
-function Comment({userID, description}) {
+
+
+function Comment({userID, description}) {  
+  
+  const {user} = useContext(Context);
+
+  const handleDelete = () => {
+    console.log(userID)
+  }
+
   return (
     <div className='comment'>
+      {(user._id === userID) && <button className='btn delete-btn' onClick={handleDelete}>x</button>}
         <UserAvatar userID={userID} />  
         <div className="comment-description">
            <p>{description}</p>
